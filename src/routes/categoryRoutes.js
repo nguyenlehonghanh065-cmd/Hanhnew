0e2a3f18155baa11aff097f133f4d4b2934088cc
nguyenlehonghanh065-cmd <nguyenlehonghanh065@gmail.com>
2026-09-19 13:10:30 +0700

    Lưu code backend lần đầu

---
+++ src/routes/categoryRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { getCategories, createCategory, deleteCategory } =
+require('../controllers/categoryController');
+router.get('/', getCategories);
+router.post('/', createCategory);
+router.delete('/:id', deleteCategory);
+module.exports = router;
